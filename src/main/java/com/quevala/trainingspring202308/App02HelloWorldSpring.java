dcd8ba930ac7ccbd45c2d8a00cddec30854a8bd4
QuevalA <87530892+QuevalA@users.noreply.github.com>
2023-08-08 12:02:37 +0200

    Test different Bean usage
    - [x] hard definition
    - [x] method call
    - [x] parameters definition

--- src/main/java/com/quevala/trainingspring202308/App02HelloWorldSpring.java
+++ src/main/java/com/quevala/trainingspring202308/App02HelloWorldSpring.java
@@ -8,6 +8,13 @@ public class App02HelloWorldSpring {
 
         var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 
-        System.out.println(context.getBean("name"));
+        System.out.println("var name = " + context.getBean("name"));
+        System.out.println("var age = " + context.getBean("age") + "\n");
+        //System.out.println("Address class call = " + context.getBean(Address.class) + "\n");
+        System.out.println("Object address0 = \n" + context.getBean("address0") + "\n");
+        System.out.println("Object person0 = \n" + context.getBean("person0") + "\n");
+        System.out.println("Object person1 = \n" + context.getBean("person1") + "\n");
+        System.out.println("Object person2 = \n" + context.getBean("person2MethodCall") + "\n");
+        System.out.println("Object person3 = \n" + context.getBean("person3Parameters"));
     }
 }
--- src/main/java/com/quevala/trainingspring202308/HelloWorldConfiguration.java
+++ src/main/java/com/quevala/trainingspring202308/HelloWorldConfiguration.java
@@ -3,11 +3,54 @@ package com.quevala.trainingspring202308;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person (String name, int age, Address address) {}
+
+record Address (String firstLine, String city) {}
+
 @Configuration
 public class HelloWorldConfiguration {
 
     @Bean
     public String name() {
-        return "Ranga";
+        return "Léon";
+    }
+
+    @Bean
+    public int age() {
+        return 26;
+    }
+
+    @Bean
+    public Person person0() {
+        var person0 = new Person("Gaspard", 29, address0());
+        return person0;
+    }
+
+    @Bean
+    public Person person1() {
+        var person1 = new Person("Jean", 20, new Address("rue du Pont", "Paris"));
+        return person1;
+    }
+
+    @Bean
+    public Person person2MethodCall() {
+        return new Person(name(), age(), address0());
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address address1) {
+        return new Person(name, age, address1);
+    }
+
+    @Bean
+    public Address address0() {
+        var address0 = new Address("avenue Charles de Gaulle", "Lyon");
+        return address0;
+    }
+
+    @Bean
+    public Address address1() {
+        var address1 = new Address("boulevard de la Paix", "Rouen");
+        return address1;
     }
 }
